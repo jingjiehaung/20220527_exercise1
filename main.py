be0113ed58cc6489769c4eee5b456c28f70057fa
jingjiehaung <47046706+jingjiehaung@users.noreply.github.com>
2022-05-27 11:31:08 +0800

    加入exercise

--- main.py
+++ main.py
@@ -6,14 +6,58 @@ def on_button_pressed_a():
                 # . # . .
                 . # . . .
     """)
+    basic.pause(200)
+    basic.show_leds("""
+        # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+    """)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    while True:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+        """)
+        basic.pause(200)
+        basic.show_leds("""
+            . . . . .
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        . . . . .
+        """)
+        basic.pause(200)
+        basic.show_leds("""
+            # . . . #
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        # . . . #
+        """)
+        basic.pause(200)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     basic.show_leds("""
-                # . . . #
+        # . . . #
                 . # . # .
                 . . # . .
                 . # . # .
                 # . . . #
+    """)
+    basic.pause(200)
+    basic.show_leds("""
+        . . . . .
+                # # # # #
+                . . . . .
+                # # # # #
+                . . . . .
     """)
 input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -6,13 +6,57 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
                 # . # . .
                 . # . . .
     `)
+    basic.pause(200)
+    basic.showLeds(`
+        # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+    `)
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    while (true) {
+        basic.showLeds(`
+            . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+        `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        . . . . .
+        `)
+        basic.pause(200)
+        basic.showLeds(`
+            # . . . #
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        # . . . #
+        `)
+        basic.pause(200)
+    }
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     basic.showLeds(`
-                # . . . #
+        # . . . #
                 . # . # .
                 . . # . .
                 . # . # .
                 # . . . #
+    `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+                # # # # #
+                . . . . .
+                # # # # #
+                . . . . .
     `)
 })
